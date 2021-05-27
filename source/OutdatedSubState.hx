71310259103606b2add36ba875bfaeed154eb52f
AlmostComical <84934074+AlmostComical@users.noreply.github.com>
2021-05-27 15:14:56 -0400

    Add files via upload

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,44 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"HEY! Press Space to check out the creator's YT channel!"
+			+ "Or press ESCAPE to ignore this!"
+			+ "\nHave Fun and Stay Cool!",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("https://www.youtube.com/channel/UCtmcB2grbl5UW6pppdjX5Cg/videos");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
